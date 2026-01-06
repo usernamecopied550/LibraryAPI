e325778c54a1e3d46831148601de3853dfec876e
rania <username.copied550@gmail.com>
2026-01-06 12:31:55 +0100

    Add files via upload

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LibraryApi.Models;
+using System.Collections.Generic;
+
+namespace LibraryApi.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors => Set<Author>();
+        public DbSet<Book> Books => Set<Book>();
+        public DbSet<Copy> Copies => Set<Copy>();
+    }
+}
