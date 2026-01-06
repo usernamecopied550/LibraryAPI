b2d6f00e75e7e0de3da4a94b42ace49804a29724
rania <username.copied550@gmail.com>
2026-01-06 12:34:44 +0100

    Add files via upload

---
+++ DTOs/BookCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryApi.DTOs
+{
+    public class BookCreateDto
+    {
+        public string Title { get; set; } = null!;
+        public int Year { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
---
+++ DTOs/BookReadDto.cs
@@ -0,0 +1,12 @@
+﻿using LibraryApi.Models;
+
+namespace LibraryApi.DTOs
+{
+    public class BookReadDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public int Year { get; set; }
+        public Author Author { get; set; } = null!;
+    }
+}
